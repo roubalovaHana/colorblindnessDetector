4ad4b8b811db0d487f0cd5fbf305b102cb4f93b2
Hana Roubalová <roubalova.hana@centrum.cz>
2023-04-25 13:01:56 +0200

    added colorblindness simulation

--- SourceCode/facade.py
+++ SourceCode/facade.py
@@ -2,6 +2,7 @@ import image_format_converter
 import kmeans
 import deltaE_distance_calculator
 from PIL import Image
+from colorblind import colorblind
 
 
 def find_issues(image_file_path):
@@ -9,6 +10,14 @@ def find_issues(image_file_path):
     graph = Image.open(image_file_path)
     grouped_colors = kmeans.group_colors(graph)
     distance_matrix = deltaE_distance_calculator.count_deltaE_distance(grouped_colors)
+    # simulate protanopia
+    simulated_protanopia = colorblind.simulate_colorblindness([grouped_colors], colorblind_type='protanopia')[0]
+    simulated_deuteranopia = colorblind.simulate_colorblindness([grouped_colors], colorblind_type='deuteranopia')[0]
+    simulated_tritanopia = colorblind.simulate_colorblindness([grouped_colors], colorblind_type='tritanopia')[0]
+    kmeans.visualize_colors(grouped_colors)
+    kmeans.visualize_colors(simulated_protanopia)
+    kmeans.visualize_colors(simulated_deuteranopia)
+    kmeans.visualize_colors(simulated_tritanopia)
 
 
-find_issues("../TestFiles/unfriendly_colormap.jpg")
+find_issues("../TestFiles/colorblind_unfriendly.jpg")
